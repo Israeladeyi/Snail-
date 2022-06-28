0fed52e6700a504351afc7eb5630a2d888eb43af
Adeyi Israel <adeyiisreal3@gmail.com>
2022-06-28 17:21:49 +0100

    Create Problem.js

---
+++ Problem.js
@@ -0,0 +1,46 @@
+for (var i =5 ; i <32; i++) {
+
+  if (i == 5) {
+
+    break;
+
+  } 
+
+  else if(i == 10){
+
+    break;
+
+  }
+
+    else if(i == 15){
+
+    break;
+
+  }
+
+    else if(i == 20){
+
+    break;
+
+  }
+
+    else if(i == 25){
+
+    break;
+
+  }
+
+    else if(i == 30){
+
+    break;
+
+  }
+
+  else {
+
+    
+
+  }
+
+}
+
